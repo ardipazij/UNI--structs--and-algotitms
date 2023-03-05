20d782888ec1a33136816044f6bc29f3ea3861cd
ardipazij <teddy.remus.edward.lupin@gmail.com>
2023-03-05 14:54:05 +0300

    Implementation of adding an element

--- 4-th_lab/linked_list.c
+++ 4-th_lab/linked_list.c
@@ -6,3 +6,38 @@ int is_empty(Node list){
 int is_full(Node list){
     return (list.size == NMAX - 1);
 }
+int get_number(int * n){
+    int flag_error = 0;
+    if(scanf("%d", n)!= 1){
+        flag_error = 1;
+        scanf("%*[^\n]");
+    }
+    return flag_error;
+}
+void push(Node * list){
+    if (is_full(*list)){
+        printf("List is full\n");
+        return;
+    }
+    int n;
+    if(get_number(&n)){
+        printf("Number's error\n");
+    }
+    else{
+        if(is_empty(*list)){
+            list->data[0] = n;
+        }
+        else{
+            for(int i = 0; i < list->size; i++){
+                if(list->data[i] > n){
+                    for(int j = list->size; j > i; j--){
+                        list->data[j] = list->data[j-1];
+                    }
+                    list->data[i] = n;
+                    break;
+                }
+            }
+        }
+        list->size++;
+    }
+}
--- 4-th_lab/linked_list.h
+++ 4-th_lab/linked_list.h
@@ -1,10 +1,12 @@
 #ifndef LINKED_LIST_H
 #define LINLED_LIST_h
-const int NMAX = 10;
+#define NMAX 10
 typedef struct {
     int data[NMAX];
     int size;
 } Node;
 int is_empty(Node list);
 int is_full(Node list);
+void push(Node * list);
+int get_number(int * n);
 #endif
