89130cc426cafb22567e67ba7e3c0c286d9e527c
ardipazij <teddy.remus.edward.lupin@gmail.com>
2023-03-06 04:44:16 +0300

    Prototype menu

---
+++ 4-th_lab/menu.h
@@ -0,0 +1,9 @@
+#include "linked_list.h"
+#ifndef MENU_H
+#define MENU_H
+void status(Node list);
+void find(Node list);
+void add_elem(Node * list);
+void delete_elem(Node * list);
+void delete_list(Node * list);
+#endif
