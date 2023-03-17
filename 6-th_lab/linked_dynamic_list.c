589eb04892d3960a10201929890d114a7f4703e8
ardipazij <teddy.remus.edward.lupin@gmail.com>
2023-03-17 14:46:35 +0300

    Impl create and destroy

--- 6-th_lab/linked_dynamic_list.c
+++ 6-th_lab/linked_dynamic_list.c
@@ -1,3 +1,36 @@
 #include "linked_dynamic_list.h"
 #include <stdio.h>
 #include "menu.h"
+#include <stdlib.h>
+List * create(){
+    List *new_stack;
+    new_stack = calloc(1, sizeof(struct List));
+    if( new_stack == NULL){
+        printf("Out of memory\n");
+        exit(1);
+    }
+    new_stack->next_elem = NULL;
+    // printf("Создан новый элемент стека\n");
+    return new_stack;
+}
+int is_empty(List * list){
+    int flag;
+    if (list->next_elem == NULL){
+        flag = 1;
+    } else {
+        flag = 0;
+    }
+    return flag;
+}
+void delete_node(List ** list){
+    free(*list);
+    //*tmp = NULL;
+}
+void destroy(List ** list){
+    List * tmp = *list;
+    while(tmp != NULL && (*list) != NULL){
+        *list = (*list)->next_elem;
+        delete_node(&tmp);
+        tmp = (*list);
+    }
+}
--- 6-th_lab/linked_dynamic_list.h
+++ 6-th_lab/linked_dynamic_list.h
@@ -1,4 +1,11 @@
 #ifndef LINKED_DYNAMIC_LIST_H
 #define LINKED_DYNAMIC_LIST_H
-
+typedef struct{
+    int data;
+    List * next_elem;
+} List;
+List * create();
+int is_empty(List * list);
+void delete_node(List ** list);
+void destroy(List ** list);
 #endif
