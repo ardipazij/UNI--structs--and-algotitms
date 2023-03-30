472843685424256e7588250b93960f0583d6ad8b
ardipazij <teddy.remus.edward.lupin@gmail.com>
2023-03-30 20:05:52 +0300

    Prototype tree

--- 9-th_lab/binary_tree.h
+++ 9-th_lab/binary_tree.h
@@ -1,3 +1,9 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
+typedef struct Tree_node{
+    int data;
+    struct Tree_node * right;
+    struct Tree_node * left;
+}Tree_node;
+void destroy(Tree_node ** head);
 #endif
