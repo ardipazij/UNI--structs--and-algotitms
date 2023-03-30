f05c64e3195b95702414e74faa3a951780517973
ardipazij <teddy.remus.edward.lupin@gmail.com>
2023-03-30 20:06:31 +0300

    Impl destroy

--- 9-th_lab/binary_tree.c
+++ 9-th_lab/binary_tree.c
@@ -1,1 +1,12 @@
 #include "binary_tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+void destroy(Tree_node ** head)
+{
+	if ((*head) == NULL)
+		return;
+
+	destroyTree((*head)->left);
+	destroyTree((*head)->right);
+	free(*head);
+}
